f69845092b0888091a57e91e9c6a2aeb2db34cfb
rsiritom <ricardo.a.siri@gmail.com>
2020-07-09 14:19:04 -0300

    Updates

---
+++ precache-manifest.6f1de52ae31beca8bff8963869cc6865.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcf2b28a54227db99a7fd7ee7fa4ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97c6bfd8364d8f5d9ab3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4d02398b82d8b830a32b",
+    "url": "/static/css/main.a6f34757.chunk.css"
+  },
+  {
+    "revision": "97c6bfd8364d8f5d9ab3",
+    "url": "/static/js/2.7db429f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7db429f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d02398b82d8b830a32b",
+    "url": "/static/js/main.e1191ad5.chunk.js"
+  },
+  {
+    "revision": "739b72af7a34c1503a96",
+    "url": "/static/js/runtime-main.e4dc104e.js"
+  },
+  {
+    "revision": "63c6969d67529b879b565d8176498a07",
+    "url": "/static/media/galeria-1.63c6969d.jpg"
+  },
+  {
+    "revision": "653e865f659b73047abb5760b7d61e40",
+    "url": "/static/media/galeria-2.653e865f.jpg"
+  },
+  {
+    "revision": "189e6536b8e004a83b15289be13c57a4",
+    "url": "/static/media/galeria-3.189e6536.jpg"
+  },
+  {
+    "revision": "f03b8eb56a4eeeb4f0635368b37795b7",
+    "url": "/static/media/galeria-4.f03b8eb5.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mundopatin/precache-manifest.bf5cab245e4c4d07759badb833d7c4b3.js"
+  "/precache-manifest.6f1de52ae31beca8bff8963869cc6865.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mundopatin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
